b50f7b4bf01cbb51875a297cdd450e9ec26087ca
Carlo Dandan <carlodandan.personal@proton.me>
2026-08-03 02:30:31 +0800

    update UA
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const BASE = "https://reanime.to";
 export const FLIX = "https://flixcloud.cc";
-export const _UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
+export const _UA = "Mozilla/5.0 (Linux; Android 15; Pixel 9) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Mobile Safari/537.36";
 export const HEADERS = { "User-Agent": _UA, "Accept": "application/json, */*" };
